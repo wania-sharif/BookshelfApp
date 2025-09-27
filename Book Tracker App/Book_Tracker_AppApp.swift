96645c16fa5687f2a2c74ec462745479e16bd148
Wania Sharif <w0842810@myscc.ca>
2025-09-27 16:07:59 -0400

    Initial Commit

---
+++ Book Tracker App/Book_Tracker_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_Tracker_AppApp.swift
+//  Book Tracker App
+//
+//  Created by Wania Sharif on 2025-09-27.
+//
+
+import SwiftUI
+
+@main
+struct Book_Tracker_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Book Tracker App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Book Tracker App
+//
+//  Created by Wania Sharif on 2025-09-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
